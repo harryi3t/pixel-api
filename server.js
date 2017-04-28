64f63b755ffdbb5b82f6c4b16916f40b70bea2e8
harryi3t <66hari@gmail.com>
2017-04-29 01:45:17 +0530

    adds express-fileupload middleware to allow fileupload

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 var path = require('path');
- var express = require('express');
- var app = express();
+var fileUpload = require('express-fileupload');
+var express = require('express');
 
- app.get('/', function (req, res) {
-   res.sendFile(__dirname + '/index.html');
- });
+var app = express();
+app.use(fileUpload());
 
- app.get('/image/:id', function (req, res) {
-   console.log('get image ', req.params.id);
-   res.send('hello');
- });
+app.get('/', function (req, res) {
+ res.sendFile(__dirname + '/index.html');
+});
 
- app.post('/image', function (req, res) {
-   res.send('image posted');
- });
+app.get('/image/:id', function (req, res) {
+ console.log('get image ', req.params.id);
+ res.send('hello');
+});
 
- app.listen(3000, function () {
-   console.log('Example app listening on port 3000!');
- });
+app.post('/image', function (req, res) {
+  if (!res.files.selectedImage)
+    return res.send('No file selected');
+
+  var fileName = res.files.selectedImage.name;
+  var data = res.files.selectedImage.data;
+});
+
+app.listen(3000, function () {
+ console.log('Example app listening on port 3000!');
+});
